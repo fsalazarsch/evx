4a89a0f8aaa6f24f2c89b0aeea95082bdf5749ca
Felipe Salazar Schlotterbeck <fsalazar.sch@gmail.com>
2020-08-24 00:23:47 -0400

    Update and rename EchoServerProtocol.py to server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-mport asyncio
+import asyncio, crcmod
 
 
 class EchoServerProtocol(asyncio.Protocol):
@@ -11,11 +11,22 @@ class EchoServerProtocol(asyncio.Protocol):
         message = data.decode()
         print('Data received: {!r}'.format(message))
 
+        crc16_func = crcmod.predefined.mkPredefinedCrcFun("crc-16")
+        
+        crc16_res = crc16_func( message[-4: -1].encode('utf-8'))
+        crc16_res2 = crc16_func( message[-4].encode('utf-8'))
+        
+        #CRC-16 is 4 bytes, but first two are zeroes and last two are CRC-16 calculated for [codec id, number of data 2]
+        
+
+        if( str(hex(crc16_res)) ==  '00'+str(crc16_func('08'.encode('utf-8')))+str(hex(crc16_res2)) ):
+            print("Chequeado")
+        
         print('Send: {!r}'.format(message))
         self.transport.write(data)
 
-        print('Close the client socket')
-        self.transport.close()
+        #print('Close the client socket')
+        #self.transport.close()
 
 
 async def main():
@@ -26,6 +37,9 @@ async def main():
     server = await loop.create_server(
         lambda: EchoServerProtocol(),
         '127.0.0.1', 8888)
+
+
+
 
     async with server:
         await server.serve_forever()
